662adbcd3e385a1a12930ef823085af4f1170b41
assumpta24 <uwamariyaassumpta24@gmail.com>
2023-03-28 13:56:56 -0700

    Updates

--- main.h
+++ main.h
@@ -21,6 +21,7 @@
 /**
  * struct fmt - Struct op
  *
+ *
  * @fmt: The format.
  * @fn: The function associated.
  */
@@ -115,4 +116,3 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
-
